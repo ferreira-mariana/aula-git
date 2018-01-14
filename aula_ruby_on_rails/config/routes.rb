d2dca4a112cfee9113b1da436e0f58d6febba630
ferreira-mariana <ferreira_mariana@id.uff.br>
2018-01-14 13:36:01 -0200

    criei a pagina Contato

--- aula_ruby_on_rails/config/routes.rb
+++ aula_ruby_on_rails/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'home', to:"static_pages#index", as: :home
   get 'sobre', to:"static_pages#about", as: :about
+  get 'contato', to:"static_pages#contact", as: :contact
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
