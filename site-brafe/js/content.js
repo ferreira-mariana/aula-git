bd4e0ac6f5620124cdada086f24adbf6df0e81dd
ferreira-mariana <ferreira_mariana@id.uff.br>
2018-01-17 21:31:19 -0200

    js: fadeIn e fadeOut no amor e perfeicao

--- site-brafe/js/content.js
+++ site-brafe/js/content.js
@@ -1,5 +1,18 @@
 $(document).ready( function() {
 
+$('.sobre-item h3').fadeOut();
+
+	$(window).scroll( function() {
+		var dist = $(window).scrollTop();
+
+		if(dist > 100){
+			$('.sobre-item h3').fadeIn();
+		}
+		else{
+			$('.sobre-item h3').fadeOut();
+		}
+	
+	});
 
 	$('.back-to-top').click( function() {
 		$('html body').animate({scrollTop: 0}, 250);
